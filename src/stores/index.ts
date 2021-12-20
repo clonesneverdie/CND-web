f5d25bec92c7ed9fd508db8e09421a8cbf5ff971
elixirevo <elixirevocation@gmail.com>
2021-12-20 20:29:55 +0900

    fix

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -68,10 +68,10 @@ export const V2AutoMiningContract = readable('0x5093F55B56e5c12663E8C08A80fc0545
 export const myV2TotalClaimable:any = writable(0)
 
 // Quickswap LP Token contract
-export const QuickswapLPContract = readable('0x1169371f8cab5c2b4592c6cddb7a3ad23ccf10b0')
+export const QuickswapLPContract = readable('')
 export const myQLPTokenBalance:any = writable(0)
 
 // Quickswap Airdrop Pool Contract
-export const QuickswapStakingPoolContract = readable('0x0087b27c49d9dd5ab59F59F8b08DB1fd70fa7c64')
+export const QuickswapStakingPoolContract = readable('')
 export const myQLPClaimable:any = writable(0)
 export const myStakedQLP:any = writable(0)
